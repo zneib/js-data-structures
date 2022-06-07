edf0b0a32f311fdf8591bb6cdde75b48fc48a7de
Zach Neibaur <zneibaur@protonmail.com>
2022-06-07 17:12:22 -0600

    Linked lists complete

--- linked-lists.js
+++ linked-lists.js
@@ -24,6 +24,56 @@ class LinkedList {
     }
   }
 
+  insertAfter(value, afterValue) {
+    const existingNode = this.find(afterValue);
+
+    if (existingNode) {
+      const newNode = {value: value, next: existingNode.next};
+      existingNode.next = newNode;
+    }
+  }
+
+  find(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return;
+    }
+
+    while (this.head && this.head.value === value) {
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        currentNode.next = currentNode.next.next;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    if (this.tail.value === value) {
+      this.tail = currentNode;
+    }
+  }
+
   toArray() {
     const elements = [];
     let currentNode = this.head;
@@ -40,8 +90,24 @@ const linkedListOne = new LinkedList();
 linkedListOne.append(1);
 linkedListOne.append('Hello There');
 linkedListOne.append('Zach');
+linkedListOne.append('Zach');
 linkedListOne.append(true);
 linkedListOne.append(18.50);
 linkedListOne.prepend('First Value');
+linkedListOne.prepend('First Value');
 
 console.log(linkedListOne.toArray());
+
+linkedListOne.delete('Zach');
+linkedListOne.delete('First Value');
+linkedListOne.delete(18.50);
+
+console.log(linkedListOne.toArray());
+console.log(linkedListOne.find('Zach'));
+console.log(linkedListOne.find('Hello There'));
+
+linkedListOne.insertAfter('New Value - 1', 1);
+linkedListOne.insertAfter('New Value - 2', 'Hello There');
+
+console.log(linkedListOne.toArray());
+
